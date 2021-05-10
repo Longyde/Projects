320d3acb61ccf7e3b25725922b42dd648a63144f
Longyde <77998284+Longyde@users.noreply.github.com>
2021-05-09 20:59:11 -0700

    Expanding my CSharp portfolio
    Added a MVC related folder.
    Added my dictionary project.

---
+++ CS/MVC_Related/Sports/Data/Migrations/20210504062358_initsetup.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Sports.Data.Migrations
+{
+    public partial class initsetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Sport",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Sports = table.Column<string>(nullable: false),
+                    Team = table.Column<string>(nullable: false),
+                    Player = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sport", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Sport");
+        }
+    }
+}
---
+++ CS/MVC_Related/Sports/Models/Sport.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Sports.Models
+{
+    public class Sport
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Sports { get; set; }
+        [Required]
+        public string Team { get; set; }
+        [Required]
+        public string Player { get; set; }
+        public Sport()
+        {
+
+        }
+    }
+}
